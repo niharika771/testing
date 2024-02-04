7cac301ceb1465bf4f90ea8b99186a55ed204a1a
unknown <niharika7715>
2024-02-05 00:46:06 +0530

    commited practise.py file and changed the branch

---
+++ practise.py
@@ -0,0 +1,10 @@
+git push -u origin main
+
+public class Main {
+  public static void main(String[] args) {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;    
+    System.out.println(isJavaFun);
+    System.out.println(isFishTasty);
+  }
+}
